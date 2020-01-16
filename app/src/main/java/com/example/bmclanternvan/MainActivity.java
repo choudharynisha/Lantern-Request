d5ee11549fe4efc1fca762771f0209e544b2732d
Nisha Choudhary <nchoudhary@brynmawr.edu>
2020-01-16 14:08:31 -0500

    Added dropdown functionality

--- app/src/main/java/com/example/bmclanternvan/MainActivity.java
+++ app/src/main/java/com/example/bmclanternvan/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.bmclanternvan.MapsActivity;
 import com.example.bmclanternvan.R;
 
 public class MainActivity extends AppCompatActivity {
     Button request;
+    Spinner dropOffSpinner;
+    Spinner pickUpSpinner;
+    String [] locations = new String[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +24,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         request = (Button)findViewById(R.id.request);
+
+        dropOffSpinner = (Spinner)findViewById(R.id.dropOffSpinner);
+        pickUpSpinner = (Spinner)findViewById(R.id.pickUpSpinner);
+
         request.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity2();
+                request(view);
             }
         });
     } // onCreate()
+
+    public void request(View view) {
+        String dropOff = dropOffSpinner.getSelectedItem().toString();
+        String pickUp = pickUpSpinner.getSelectedItem().toString();
+
+        locations[0] = pickUp;
+        locations[1] = dropOff;
+        Log.i("pickup", "the pickup location is: " + pickUp + ", and drop off is: " + dropOff);
+        openActivity2();
+    }
 
     public void openActivity2() {
         Intent intent = new Intent(this, MapsActivity.class);
